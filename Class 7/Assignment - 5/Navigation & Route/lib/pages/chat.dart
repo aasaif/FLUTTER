42bc13d896c1d2c5a0fa8189b1f3ec54a31c2d81
aasaif <adnan.saif162@northsouth.edu>
2022-07-01 10:28:56 +0600

    file uploaded

--- Class 7/Assignment - 5/Navigation & Route/lib/pages/chat.dart
+++
@@ -1,9 +0,0 @@
-import 'package:ecommerce_app/details_screen.dart';
-import 'package:flutter/material.dart';
-
-class ChatScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold();
-  }
-}
